e5400404ed1e5bcf7469aaf6d8412e285d3b35c6
Rodrik31 <rodrigo.cav31@gmail.com>
2023-01-24 11:08:44 -0300

    Pequenas melhorias e modificações

--- desafio_sistema_bancario.py
+++ desafio_sistema_bancario.py
@@ -27,14 +27,13 @@ while True:
             print("Erro, digite um valor positivo.")
         else:
             saldo += deposito
-            extrato += f"Depósito: R$ {deposito:.2f}\n"
-           
+            extrato += f"Depósito: R$ {deposito:.2f}\n"           
 
     elif opcao == "s":
         limpar()
         saque = float(input("Digite um valor para sacar:" ))
         if saque > 500:
-            print(f"Saque recusado. Limite de saque: R$ {limite:.2f}")
+            print(f"Saque recusado.\nLimite de saque: R$ {limite:.2f}")
         elif saque <= 0:
             print("Erro, digite um valor positivo.")
         elif saque > saldo:
@@ -46,19 +45,16 @@ while True:
             numero_saques +=1
             extrato += f"Saque: R$ {saque:.2f}\n"
 
-
-
     elif opcao == "e":
         limpar()
         if extrato:
             print(extrato + f"\nSaldo: R$ {saldo:.2f}")
         else:
             print("Não foram realizadas movimentações.")
-
-
     
     elif opcao == "q":
-        limpar()
+        input("Obrigado por utilizar nossos sistemas.")
+        limpar()        
         break
 
     else:
