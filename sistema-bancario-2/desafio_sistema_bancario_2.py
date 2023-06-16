dbb5f4108462554f34a8c77e37879f4d563e8874
Rodrik31 <rodrigo.cav31@gmail.com>
2023-06-16 20:05:42 -0300

    Commit inicial.

---
+++ sistema-bancario-2/desafio_sistema_bancario_2.py
@@ -0,0 +1,62 @@
+import os
+def limpar():
+    os.system('cls') or None
+limpar()
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        limpar()
+        deposito = float(input("Digite o valor para depositar: "))
+        if deposito <= 0:
+            print("Erro, digite um valor positivo.")
+        else:
+            saldo += deposito
+            extrato += f"Depósito: R$ {deposito:.2f}\n"           
+
+    elif opcao == "s":
+        limpar()
+        saque = float(input("Digite um valor para sacar:" ))
+        if saque > 500:
+            print(f"Saque recusado.\nLimite de saque: R$ {limite:.2f}")
+        elif saque <= 0:
+            print("Erro, digite um valor positivo.")
+        elif saque > saldo:
+            print("Erro, o valor do saque é maior que saldo existente.")
+        elif numero_saques == LIMITE_SAQUES:
+            print("O valor não pode ser sacado.\nLimite diário de saques atingindo.")
+        else:
+            saldo -= saque
+            numero_saques +=1
+            extrato += f"Saque: R$ {saque:.2f}\n"
+
+    elif opcao == "e":
+        limpar()
+        if extrato:
+            print(extrato + f"\nSaldo: R$ {saldo:.2f}")
+        else:
+            print("Não foram realizadas movimentações.")
+    
+    elif opcao == "q":
+        input("Obrigado por utilizar nossos sistemas.")
+        limpar()        
+        break
+
+    else:
+        limpar()
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
