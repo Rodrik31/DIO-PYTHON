358d1debd011d8bb540b36b790630a37bb740831
Rodrik31 <rodrigo.cav31@gmail.com>
2023-06-18 21:27:12 -0300

    Implementação das funções menu e saque.

--- sistema-bancario-2/desafio_sistema_bancario_2.py
+++ sistema-bancario-2/desafio_sistema_bancario_2.py
@@ -3,60 +3,82 @@ def limpar():
     os.system('cls') or None
 limpar()
 
-menu = """
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
-=> """
+def menu():    
+    print("""
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+    => """)
+    opcao = input()
+    return opcao
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
-
-while True:
-
-    opcao = input(menu)
-
-    if opcao == "d":
-        limpar()
-        deposito = float(input("Digite o valor para depositar: "))
-        if deposito <= 0:
-            print("Erro, digite um valor positivo.")
-        else:
-            saldo += deposito
-            extrato += f"Depósito: R$ {deposito:.2f}\n"           
-
-    elif opcao == "s":
-        limpar()
-        saque = float(input("Digite um valor para sacar:" ))
-        if saque > 500:
-            print(f"Saque recusado.\nLimite de saque: R$ {limite:.2f}")
-        elif saque <= 0:
-            print("Erro, digite um valor positivo.")
-        elif saque > saldo:
-            print("Erro, o valor do saque é maior que saldo existente.")
-        elif numero_saques == LIMITE_SAQUES:
-            print("O valor não pode ser sacado.\nLimite diário de saques atingindo.")
-        else:
-            saldo -= saque
-            numero_saques +=1
-            extrato += f"Saque: R$ {saque:.2f}\n"
-
-    elif opcao == "e":
-        limpar()
-        if extrato:
-            print(extrato + f"\nSaldo: R$ {saldo:.2f}")
-        else:
-            print("Não foram realizadas movimentações.")
-    
-    elif opcao == "q":
-        input("Obrigado por utilizar nossos sistemas.")
-        limpar()        
-        break
-
+def saque(*, saldo, valor, extrato, limite, numero_saque, limite_saque):
+    if valor > 500:
+        print(f"Saque recusado.\nLimite de saque: R$ {limite:.2f}")
+    elif valor <= 0:
+        print("Erro, digite um valor positivo.")
+    elif valor > saldo:
+        print("Erro, o valor do saque é maior que saldo existente.")
+    elif numero_saque == limite_saque:
+        print("O valor não pode ser sacado.\nLimite diário de saques atingindo.")
     else:
+        saldo -= valor
+        numero_saque +=1
+        extrato += f"Saque: R$ {valor:.2f}\n"
         limpar()
-        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("Saque realizado com sucesso.")
+
+    return saldo, extrato, numero_saque
+
+def main():    
+    saldo = 0
+    valor = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+
+        opcao = menu()
+
+        if opcao == "d":
+            limpar()
+            deposito = float(input("Digite o valor para depositar: "))
+            if deposito <= 0:
+                print("Erro, digite um valor positivo.")
+            else:
+                saldo += deposito
+                extrato += f"Depósito: R$ {deposito:.2f}\n"           
+
+        elif opcao == "s":
+            limpar()
+            valor = float(input("Digite um valor para sacar:" ))
+            saldo, extrato, numero_saques = saque(
+                                                    saldo=saldo, 
+                                                    valor=valor, 
+                                                    extrato=extrato, 
+                                                    limite=limite, 
+                                                    numero_saque=numero_saques, 
+                                                    limite_saque=LIMITE_SAQUES
+                                                )
+            
+
+        elif opcao == "e":
+            limpar()
+            if extrato:
+                print(extrato + f"\nSaldo: R$ {saldo:.2f}")
+            else:
+                print("Não foram realizadas movimentações.")
+        
+        elif opcao == "q":
+            input("Obrigado por utilizar nossos sistemas.")
+            limpar()        
+            break
+
+        else:
+            limpar()
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+main()
