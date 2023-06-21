9f506e01a2c3be8b24812a96b9d6b9505f733b5a
Rodrik31 <rodrigo.cav31@gmail.com>
2023-06-20 23:21:25 -0300

    Implementação e integração das funções criar conta corrente, filtrar usuários e listar conta corrente. Pequenas modificações para melhorar usabilidade e exibição do conteúdo, além do tratamento e verificação da entrada de dados.

--- sistema-bancario-2/desafio_sistema_bancario_2.py
+++ sistema-bancario-2/desafio_sistema_bancario_2.py
@@ -9,10 +9,10 @@ def menu():
     [s] Sacar
     [e] Extrato
     [c] Cadastrar usuário
-    [l] Listar usuários
-    [q] Sair
-    => """)
-    opcao = input()
+    [cc] Criar Conta Corrente
+    [lc] Listar contas
+    [q] Sair""")
+    opcao = input("\nEscolha uma operação: ")
     return opcao
 
 def saque(*, saldo, valor, extrato, limite, numero_saque, limite_saque):
@@ -52,17 +52,16 @@ def exibir_extrato(saldo, /, *, extrato):
         print("Não foram realizadas movimentações.") 
     return extrato
 
-def cadastrar_usuario(nome, usuarios):
+def cadastrar_usuario(*, nome, usuarios, contas):
     limpar()
-    CPF = input("Informe um CPF para cadastrar.\nNão use espaços, pontos ou traços.\nUse somente números.")
-    if len(CPF) is not 11:
-        print("Digite um CPF válido.")
-        return usuarios
+    CPF = input("Informe um CPF para cadastrar.\nNão use espaços, pontos ou traços.\nUse somente números.\n>>>")
+    if CPF:
+        resultado = filtrar_usuarios(CPF, usuarios)
+        if not resultado:
+            return
     else:
-        for usuario in usuarios:
-            if usuario["CPF"] == CPF:
-                print("Usuário já cadastrado com o CPF fornecido.")
-                return usuarios   
+        return
+    
     nome_completo = input("Informe seu nome completo: ")
     data_nascimento = input("Informe sua data de nascimento: ")
     endereco = input("Informe seu endereço no seguinte modelo:\nlogradouro - nro - bairro - cidade/sigla do estado")
@@ -74,22 +73,46 @@ def cadastrar_usuario(nome, usuarios):
         "endereço":endereco
                     }
     usuarios.append(novo_usuario)
+    criar_conta_corrente(novo_usuario, contas)
+    limpar()
+    return usuarios
 
-    return novo_usuario, usuarios
+def filtrar_usuarios(CPF, usuarios):
+    if len(CPF) is not 11:
+        print("Digite um CPF válido.")
+        return False
+    else:
+        for usuario in usuarios:
+            if usuario["CPF"] == CPF:
+                print("Usuário já cadastrado com o CPF fornecido.")
+                return False
+
 
 def criar_conta_corrente(usuario, contas):
     numero_conta = len(contas) + 1
     nova_conta = {
-        "usuario":usuario,
-        "número_da_conta":numero_conta,
-        "número_da_agência":"0001",}
-    contas.append(nova_conta)        
+        "usuário":usuario["nome"],
+        "nome":usuario["nome_completo"],
+        "número_conta":numero_conta,
+        "agência":"0001",}
+    
+    contas.append(nova_conta)          
     return contas
 
-def listar_usuarios(usuarios):
+def listar_contas(contas):
     limpar()
-    for usuario in usuarios:        
-        print(usuario)
+    if not contas:
+        print("Não existem contas cadastradas.")
+        return        
+    for conta in contas:  
+        dados = f"""\n
+        Agência:\t{conta["agência"]}
+        C/C:\t\t{conta["número_conta"]}
+        Títular:\t{conta["nome"]}            
+        """        
+        print(dados)
+    input()
+    limpar()
         
 
 def main():    
@@ -125,10 +148,22 @@ def main():
             extrato = exibir_extrato(saldo, extrato=extrato)
         elif opcao == "c":
             nome = input("Informe o nome do usuário: ")
-            novo_usuario, usuarios = cadastrar_usuario(nome, usuarios)
-            contas = criar_conta_corrente(novo_usuario, contas)
-        elif opcao == "l":
-            listar_usuarios(usuarios)  
+            usuarios = cadastrar_usuario(nome=nome, usuarios=usuarios, contas=contas)            
+        elif opcao == "lc":
+            listar_contas(contas)  
+        elif opcao == "cc":
+            limpar()
+            CPF = input("Informe um CPF para verificação.\nNão use espaços, pontos ou traços.\nUse somente números.\n>>>")            
+            cpf_existe = False
+            for usuario in usuarios:
+                if usuario["CPF"] == CPF:
+                    cpf_existe = True
+                    contas = criar_conta_corrente(usuario, contas)                    
+                    break
+
+            if not cpf_existe:
+                print("CPF não encontrado nos usuários.\nCadastre um usuário primeiro, antes de criar uma conta.")            
+
         elif opcao == "q":
             input("Obrigado por utilizar nossos sistemas.")
             limpar()        
